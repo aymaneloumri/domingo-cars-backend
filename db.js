143b31f98948974e38212ff1468a1d6e48012cd5
aymaneloumri <ayman.eloumri@gmail.com>
2026-06-02 01:55:24 +0200

    fix: db.js add debug logs

--- db.js
+++ db.js
@@ -1,8 +1,20 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg')
+
+if (!process.env.DATABASE_URL) {
+  console.error('DATABASE_URL is not set!')
+}
+
+console.log('Connecting to DB:', process.env.DATABASE_URL ? 'URL found' : 'NO URL')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
-module.exports = pool;
+pool.on('error', (err) => {
+  console.error('DB Pool error:', err.message)
+})
+
+module.exports = pool
